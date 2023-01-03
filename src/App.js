d9c1b739434447c203d2c18f13e7cb0af85ed097
Dahlia <rlally89@gmail.com>
2023-01-03 12:09:28 -0800

    mapping over todos, still hardcoded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import ToDoItem from './components/ToDoItem';
 
-const todos = ['walk the dog', 'grocery shop', 'portfolio updates']
+const todos = ['walk the dog', 'grocery shop', 'portfolio updates', 'put ski gear away', 'chop wood']
 
 function App() {
   return (
     <div>
       <h1>Welcome</h1>
       <h2>To Do List:</h2>
+      {todos.map(todo => <ToDoItem todo={todo} />)}
     </div>
   );
 }
---
+++ src/components/ToDoItem.js
@@ -0,0 +1,14 @@
+
+
+
+
+export default function ToDoItem({todo}) {
+    console.log(todo)
+
+    return (
+        <div>
+            <h3>{todo}</h3>
+        </div>
+    )
+
+}
